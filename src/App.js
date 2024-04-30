e9c6b06c54f478e22d777cf226331e4acdf5dc80
kepo402 <alawodeolayinka2@gmail.com>
2024-04-30 02:03:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import VideoPlayer from './VideoPlayer';
+import VRPlayer from './VRPlayer';
 
 function App() {
+  const [isInVR, setIsInVR] = useState(false);
+
+  const toggleVRMode = () => {
+    setIsInVR(!isInVR);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isInVR ? (
+        <VRPlayer onExitVR={toggleVRMode} />
+      ) : (
+        <VideoPlayer onEnterVR={toggleVRMode} />
+      )}
     </div>
   );
 }
